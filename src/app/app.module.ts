677491db63faf1aa7a92aa5004928261e1c0871f
Malik <Malikazhanntu@gmail.com>
2018-01-09 19:42:33 +0500

    Services done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-//two way binding k lye Forms module ko import krna ha or then imports me neechy  b add krna ha//
 import { FormsModule } from '@angular/forms';
-import { HeroDetailsComponent } from './hero-details/hero-details.component'; // <-- NgModel lives here
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import {HeroService} from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service'; // <-- NgModel lives here
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailsComponent,
-    
-  ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessagesComponent
+
+  ],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
