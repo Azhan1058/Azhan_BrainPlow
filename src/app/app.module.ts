aa34ce408c8cd390ad2508b61516a44a0064c19b
Malik <Malikazhanntu@gmail.com>
2018-01-09 16:34:30 +0500

    3rdchange

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 //two way binding k lye Forms module ko import krna ha or then imports me neechy  b add krna ha//
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details/hero-details.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailsComponent,
     
   ],
   imports: [
