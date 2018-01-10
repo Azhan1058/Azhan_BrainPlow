678ddaf9cfd36df83e9da6c9945a1bf951b4f734
Malik <Malikazhanntu@gmail.com>
2018-01-10 19:52:29 +0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-details/hero-details.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
-import { HeroDetailsComponent } from './hero-details/hero-details.component';
-import {HeroService} from './hero.service';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service'; // <-- NgModel lives here
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
-    HeroDetailsComponent,
+    HeroDetailComponent,
     MessagesComponent
-
   ],
-  providers: [HeroService, MessageService],
-  bootstrap: [AppComponent]
+  providers: [ HeroService, MessageService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,16 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
-import { Component, OnInit, Input } from '@angular/core';
-@Component({
-  selector: 'app-hero-details',
-  templateUrl: './hero-details.component.html',
-  styleUrls: ['./hero-details.component.css']
-})
-export class HeroDetailsComponent implements OnInit {
-  @Input() hero: Hero;
-  constructor() { }
+import { HeroService } from '../hero.service';
 
-  ngOnInit() {
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-details.component.html',
+  styleUrls: [ './hero-details.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('query');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
